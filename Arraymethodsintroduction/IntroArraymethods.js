af5ece22f6cf5eaae0574d7e8bf146ee79e6c5b7
sakinabanu <banusakina04@gmail.com>
2023-08-17 20:01:35 +0530

    Array Methods

--- Arraymethodsintroduction/IntroArraymethods.js
+++ Arraymethodsintroduction/IntroArraymethods.js
@@ -2,7 +2,7 @@
 JAVASCRIPT ARRAY METHODS :
 In JavaScript, Array is a built-in global object that allows you to store multiple elements at once.
 Array methods are built -in functions to JavaScript that we can apply to our arrays.
-Each method has a unique function that performs a change or calculation to our array and saves us from writing common functions from scratch.
+Each method has a unique function that performs a change or calculation to our array and saves us from writing complex functions from scratch.
 Array methods does a complex function work in simple way.
 They are 38 array methods.
 */
